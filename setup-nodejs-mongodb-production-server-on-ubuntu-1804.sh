60a1dce00fec6c563846262654bcc0c929f1bc4c
tripaider <tripaider.com@gmail.com>
2022-02-24 11:39:43 +0530

    third commit

--- setup-nodejs-mongodb-production-server-on-ubuntu-1804.sh
+++ setup-nodejs-mongodb-production-server-on-ubuntu-1804.sh
@@ -13,29 +13,29 @@ curl -sL https://deb.nodesource.com/setup_16.x | sudo -E bash -
 sudo apt-get install -y nodejs
 
 
-# echo "
-# ----------------------
-#   MONGODB
-# ----------------------
-# "
+echo "
+----------------------
+  MONGODB
+----------------------
+"
 
-# # import mongodb 4.0 public gpg key
-# sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 9DA31620334BD75D9DCB49F368818C72E52529D4
+# import mongodb 4.0 public gpg key
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 9DA31620334BD75D9DCB49F368818C72E52529D4
 
-# # create the /etc/apt/sources.list.d/mongodb-org-4.0.list file for mongodb
-# echo "deb [ arch=amd64 ] https://repo.mongodb.org/apt/ubuntu bionic/mongodb-org/4.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.0.list
+# create the /etc/apt/sources.list.d/mongodb-org-4.0.list file for mongodb
+echo "deb [ arch=amd64 ] https://repo.mongodb.org/apt/ubuntu bionic/mongodb-org/4.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.0.list
 
-# # reload local package database
-# sudo apt-get update
+# reload local package database
+sudo apt-get update
 
-# # install the latest version of mongodb
-# sudo apt-get install -y mongodb-org
+# install the latest version of mongodb
+sudo apt-get install -y mongodb-org
 
-# # start mongodb
-# sudo systemctl start mongod
+# start mongodb
+sudo systemctl start mongod
 
-# # set mongodb to start automatically on system startup
-# sudo systemctl enable mongod
+# set mongodb to start automatically on system startup
+sudo systemctl enable mongod
 
 
 echo "
